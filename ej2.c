4a26e484ebd1eb72f89b9b48b36486040485c108
Guille <150677095+guille1928@users.noreply.github.com>
2025-11-09 19:13:32 +0100

    apartado 2

--- ej2.c
+++ ej2.c
@@ -4,7 +4,7 @@
 //Apartado 1-------------------------------------------------
 //Apartado1.1
 //declaro la constante simbolica
-#define SIZE = 10;
+#define SIZE 10
 //Apartado1.2
 //declaro la estructura
 struct potencia_p {
@@ -51,14 +51,62 @@ int leer (potenciaP_t *p){
 
 }
 
+//Apartado 2-------------------------------------------------
+//Apartado 2.1
+//creo funcion para iniciar el array que no devuelve nada
+void initArrayEst (potenciaP_t  arr[]){
+for (int i=0; i<SIZE;i++){
+    arr[i].base =i+1;
+    arr[i].exp=0;
+    arr[i].potencia=1;
+}
+}
+
+//se crea funcion que recibe un array de potencia_t y printa cada valor del array
+void ComprobarArray(potenciaP_t arr[]){
+printf(" Índice");
+for (int i =0; i< SIZE; i++){
+    printf(" i:%d ",i);
+}
+printf("\n");
+printf(" base");
+for (int i =0; i<SIZE; i++){
+   printf(" %d ",arr[i].base );   
+}
+printf("\n");
+printf(" exp");
+for (int i =0; i<SIZE; i++){
+    printf (" %d ",arr[i].exp );
+}
+printf("\n");
+printf(" potencia");
+for (int i =0; i<SIZE; i++){
+    printf(" %d  ", arr[i].potencia);
+}
+printf("\n");
+
+}
+
+//creo array para printar el array 
+void printArrayEst (potenciaP_t arr[]){
+
+    
+}
+
 int main (){
 potenciaP_t potencia;
+potenciaP_t array[SIZE];
 if(setBaseExp(&potencia,4,4)==0){
     printf("Creado correntamente \n");
     setPotenciaEst(&potencia);
-    printf("Todo bene \n");
+    printf("Todo bien \n");
     leer(&potencia);
 }
+
+printf("\n Apartado 2 : \n");
+printf("Inicio y compruebo como esta el array \n");
+initArrayEst(array);
+ComprobarArray(array);
 
 
 }
